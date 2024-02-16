191718beef4abfb380f70691feba9a847429baa8
HananR99 <hananramzan199@gmail.com>
2024-02-16 15:00:30 +0530

    python file

--- animal.py
+++ animal.py
@@ -1,16 +1,9 @@
 import sys
-
-def cat():
-    print('Meow')
-
 def default():
     print('Hello')
 
 def main():
-    if sys.argv[1] == 'cat':
-        cat()
-    else:
-        default()
+    default()
 
 if __name__ == '__main__':
     main()
